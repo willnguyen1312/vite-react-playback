75b7c39b529e19b0cf96e37046731d893ac7c62a
Nam Nguyen <nam.nguyen@redspace.com>
2023-03-24 07:36:16 -0300

    ❤️‍🔥

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,12 @@
 import { defineConfig } from "vite";
 import { splitVendorChunkPlugin } from "vite";
+import mkcert from "vite-plugin-mkcert";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), splitVendorChunkPlugin()],
+  server: {
+    https: true,
+  },
+  plugins: [react(), splitVendorChunkPlugin(), mkcert()],
 });
